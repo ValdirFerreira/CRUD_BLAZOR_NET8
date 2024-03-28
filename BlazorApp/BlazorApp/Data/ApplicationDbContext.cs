e2862a9041f8a9b0edd2bd5c1a2005a66d92bdd5
Valdir Ferreira Gomes da Silva <valdir.silva@fcamara.com.br>
2024-03-28 05:41:42 -0300

    CRUD BLAZOR NET 8

---
+++ BlazorApp/BlazorApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+
+        public DbSet<Product> Prod { get; set; }
+    }
+}
